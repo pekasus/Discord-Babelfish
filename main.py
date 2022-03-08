b78238ac5e6ab5109a681c66e4b7be06dc83268b
Joshua Meyer <josh.engineer9@gmail.com>
2022-03-08 17:19:23 -0600

    test query command

--- main.py
+++ main.py
@@ -4,3 +4,26 @@ from googletrans import Translator
 from gtts import gTTS
 import os
 
+def takecommand():
+    r = sr.Recognizer()
+    with sr.Microphone() as source:
+        print("listening...")
+        r.pause_threshold = 1
+        audio = r.listen(source)
+
+    try:
+        print("Recognizing...")
+        query = r.recognize_google(audio, language='en')
+        print(f"User said: {query}\n")
+    except Exception as e:
+        print("Did not copy.")
+        return "None"
+
+    return query
+
+query = takecommand()
+while (query == "None"):
+    query = takecommand()
+
+
+
