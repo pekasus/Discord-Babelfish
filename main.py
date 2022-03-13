a82449106243fa62de1c58314d6a490634274ed0
Joshua Meyer <josh.engineer9@gmail.com>
2022-03-12 20:59:33 -0600

    functioning record audio, transcribe, translate and read in foreign language

--- main.py
+++ main.py
@@ -1,7 +1,4 @@
-import io
-
 from playsound import playsound
-import sounddevice as sd
 import speech_recognition as sr
 from speech_recognition import UnknownValueError
 from googletrans import Translator
@@ -11,12 +8,26 @@ import discord
 from discord.ext import commands
 from dotenv import load_dotenv
 import asyncio
+import wave
+
+
+SAMPLING_RATE = 48000
+CHANNELS = 2
+FRAME_WIDTH = 2
+
 
 ########################################
+# SETUP VARIABLES
+########################################
 
+lang = 'de'
+speak_length = 10
+filename = 'test1.wav'
 # output_path for MP3Sink will be available in version 2.1
 
 ########################################
+########################################
+
 
 load_dotenv()
 TOKEN = os.getenv('BOT_TOKEN')
@@ -47,51 +58,49 @@ async def on_ready():
     # Need to use .wav file for speech_recognition
     connected.start_recording(discord.sinks.WaveSink(), transcribe_audio, channel)
     print("Recording...\n")
-    await asyncio.sleep(5)
+    await asyncio.sleep(speak_length)
     print("Finished recording.\n")
     connected.stop_recording()
 
 
 async def transcribe_audio(sink, channel):
-    # user_ids = [user_id for user_id, audio in sink.audio_data.items()]
+    user_ids = [user_id for user_id, audio in sink.audio_data.items()]
     # recorded_users = [f"{user_id}" for user_id, audio in sink.audio_data.items()]
     await sink.vc.disconnect()
     files = [discord.File(audio.file, f"{user_id}.{sink.encoding}") for user_id, audio in sink.audio_data.items()]
     # await text_channel.send("Here's a recording!", files=files)
 
-    filename = 'test.wav'
-    b = io.BytesIO(files[0].fp.read())
-    with open(filename, 'wb') as f:
-        f.write(b.read())
-
-    playsound(filename)
+    f = wave.open(filename, 'w')
+    f.setframerate(SAMPLING_RATE)
+    f.setnchannels(CHANNELS)
+    f.setsampwidth(FRAME_WIDTH)
+    f.writeframes(files[0].fp.read())
+    f.writeframes(b'')
 
     r = sr.Recognizer()
     sinkfile = sr.AudioFile(filename)
-    # with sr.Microphone() as source:
+
     with sinkfile as source:
         print("listening...")
-        # r.pause_threshold = 15
-        # audio = r.listen(source)  # this listens to microphone (I think)
         audio = r.record(source)  # use record to get from file
-        print(type(audio))
-    try:
-        print("Recognizing...")
-        query = r.recognize_google(audio, language='en-US', show_all=True)
-        print(f"User said: {query}\n")
-    except UnknownValueError as e:
-        print("Did not copy.")
-        print(str(e))
-        return "None"
+        try:
+            print("Recognizing...")
+            query = r.recognize_google(audio, language='en-US', show_all=True)
+            transcript = query['alternative'][0]['transcript']
+            print(f"User said: {transcript}\n")
+        except UnknownValueError as e:
+            print("Did not copy.")
+            print(str(e))
+            return "None"
 
     translator = Translator()
 
-    text_to_translate = translator.translate(query, dest='de')
+    text_to_translate = translator.translate(transcript, dest=lang)
     text = text_to_translate.text
 
     print(text)
 
-    speak = gTTS(text=text, lang='de', slow=False)
+    speak = gTTS(text=text, lang=lang, slow=False)
     speak.save("translate.mp3")
     playsound('translate.mp3')
     os.remove('translate.mp3')
@@ -101,7 +110,6 @@ async def transcribe_audio(sink, channel):
 # query = transcribe_audio()
 # while (query == "None"):
 #     query = transcribe_audio()
-
 
 
 
